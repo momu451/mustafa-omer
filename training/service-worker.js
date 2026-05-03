2f00849d3208df8f89d46ebc6131555b04753991
momu451 <48030786+momu451@users.noreply.github.com>
2026-05-03 10:21:51 +0100

    Replace Training OS service worker from uploaded zip

--- training/service-worker.js
+++ training/service-worker.js
@@ -1,16 +1,25 @@
-const CACHE = 'soviet-training-os-v3-2026-05-02';
-const ASSETS = ['./','index.html','style.css','app.js','manifest.webmanifest','icon.svg'];
+const CACHE_NAME = 'sto-v3-cache-1';
+const urlsToCache = [
+  '/training/',
+  '/training/index.html',
+  '/training/style.css',
+  '/training/app.js',
+  '/training/manifest.webmanifest',
+  '/training/icon.svg'
+];
+
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
+
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(res => {
-    const copy = res.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
-    return res;
-  }).catch(() => caches.match('./'))));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
