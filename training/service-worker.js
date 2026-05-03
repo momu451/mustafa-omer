f48b6eacf52554c49038487d5a2bd662f5cc5991
momu451 <48030786+momu451@users.noreply.github.com>
2026-05-03 10:28:21 +0100

    Make Training OS service worker path-portable and resilient

--- training/service-worker.js
+++ training/service-worker.js
@@ -1,25 +1,38 @@
-const CACHE_NAME = 'sto-v3-cache-1';
-const urlsToCache = [
-  '/training/',
-  '/training/index.html',
-  '/training/style.css',
-  '/training/app.js',
-  '/training/manifest.webmanifest',
-  '/training/icon.svg'
+const CACHE_NAME = 'sto-v3-cache-2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icon.svg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(cached => cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      }))
+      .catch(() => caches.match('./index.html'))
   );
 });
